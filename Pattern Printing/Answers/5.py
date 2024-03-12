42c389bc935dbbd9e81cdca96cab093a5cd630e1
Gourav Lohar <gouravlohar640@gmail.com>
2024-03-12 22:47:52 +0530

    5th answer added

---
+++ Pattern Printing/Answers/5.py
@@ -0,0 +1,19 @@
+    # *
+    # **
+    # ***
+    # ****
+    # *****
+    # ****
+    # ***
+    # **
+    # *
+
+n = 5
+for i in range(n):
+    for j in range(i+1):
+        print("*", end=" ")
+    print("")
+for i in range(n-1, 0, -1):
+    for j in range(i):
+        print("*", end=" ")
+    print("")

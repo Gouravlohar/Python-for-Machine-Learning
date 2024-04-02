7fbb83783a436a8443df9432c443c5d66021480c
Gourav Lohar <gouravlohar640@gmail.com>
2024-04-02 21:25:20 +0530

    25th answer added

---
+++ Pattern Printing/Answers/25.py
@@ -0,0 +1,10 @@
+n = 5
+for i in range(1, n + 1):
+    for j in range(1, n - i + 1):
+        print(" ", end="")
+    for j in range(1, n + 1):
+        if i == 1 or j == 1 or i == n or j == n:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
